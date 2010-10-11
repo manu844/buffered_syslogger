e3aed394e23a003de75becda007420c5233572ec
Laszlo Bacsi <lackac@secretsaucepartners.com>
2010-10-11 16:27:05 +0200

    A buffered syslogger implementation based on the original syslogger

---
+++ buffered_syslogger.gemspec
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+Gem::Specification.new do |s|
+  s.name         = "buffered_syslogger"
+  s.version      = "0.1.0"
+  s.date         = "2010-10-11"
+  s.authors      = ["László Bácsi", "Cyril Rohr"]
+  s.email        = "lackac@secretsaucepartners.com"
+  s.homepage     = "http://github.com/sspinc/buffered_syslogger"
+  s.summary      = "Buffered syslogger based on the syslogger gem."
+  s.description  = <<-EOH
+A drop-in replacement for the Rails 3 default BufferedLogger library,
+that logs to syslog instead of a log file. Builds on the syslogger gem.
+  EOH
+
+  s.files        = Dir['lib/**/*','spec/**/*'] + %w(README.rdoc LICENSE)
+  s.extra_rdoc_files = %w(LICENSE README.rdoc)
+  s.rdoc_options = %w(--charset=UTF-8)
+  s.platform     = Gem::Platform::RUBY
+  s.require_path = 'lib'
+  s.rubyforge_project = 'nowarning'
+  s.required_rubygems_version = '>= 1.3.6'
+
+  s.add_runtime_dependency "activesupport", ">= 3.0.0"
+  s.add_development_dependency "rspec", ">= 2.0.0"
+end
---
+++ spec/buffered_syslogger_spec.rb
@@ -0,0 +1,82 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe BufferedSyslogger do
+
+  let(:syslog) { mock("syslog", :mask= => true) }
+
+  it "should log to the default syslog facility, with the default options" do
+    logger = BufferedSyslogger.new
+    Syslog.should_receive(:open).with($0, Syslog::LOG_PID | Syslog::LOG_CONS, nil).and_yield(syslog)
+    syslog.should_receive(:log).with(Syslog::LOG_NOTICE, "Some message")
+    logger.warn "Some message"
+  end
+
+  it "should log to the user facility, with specific options" do
+    logger = BufferedSyslogger.new("my_app", Syslog::LOG_PID, Syslog::LOG_USER)
+    Syslog.should_receive(:open).with("my_app", Syslog::LOG_PID, Syslog::LOG_USER).and_yield(syslog)
+    syslog.should_receive(:log).with(Syslog::LOG_NOTICE, "Some message")
+    logger.warn "Some message"
+  end
+
+  %w{debug info warn error fatal unknown}.each do |logger_method|
+    it "should respond to the #{logger_method.inspect} method" do
+      BufferedSyslogger.new.should respond_to logger_method.to_sym
+    end
+  end
+
+  %w{debug info warn error fatal unknown}.each do |logger_method|
+    it "should respond to the #{logger_method.inspect}? method" do
+      BufferedSyslogger.new.should respond_to "#{logger_method}?".to_sym
+    end
+  end
+
+  describe "add" do
+    before do
+      @logger = BufferedSyslogger.new("my_app", Syslog::LOG_PID, Syslog::LOG_USER)
+    end
+    it "should respond to add" do
+      @logger.should respond_to(:add)
+    end
+    it "should correctly log" do
+      Syslog.should_receive(:open).with("my_app", Syslog::LOG_PID, Syslog::LOG_USER).and_yield(syslog)
+      syslog.should_receive(:log).with(Syslog::LOG_INFO, "message")
+      @logger.add(Logger::INFO, "message")
+    end
+    it "should take the message from the block if :message is nil" do
+      Syslog.should_receive(:open).with("my_app", Syslog::LOG_PID, Syslog::LOG_USER).and_yield(syslog)
+      syslog.should_receive(:log).with(Syslog::LOG_INFO, "my message")
+      @logger.add(Logger::INFO) { "my message" }
+    end
+  end
+
+  context "when used in buffered mode" do
+    before { subject.auto_flushing = false }
+
+    it "without flushing should not send anything to syslog" do
+      Syslog.should_not_receive(:open)
+      subject.warn "Self destruction in progress..."
+    end
+
+    it "should send the messages to syslog when flushed" do
+      Syslog.should_receive(:open).and_yield(syslog)
+      syslog.should_receive(:log).with(Syslog::LOG_INFO, "Self destruction in 3...")
+      syslog.should_receive(:log).with(Syslog::LOG_NOTICE, "2..")
+      syslog.should_receive(:log).with(Syslog::LOG_WARNING, "1.")
+      subject.info "Self destruction in 3..."
+      subject.warn "2.."
+      subject.error "1."
+      subject.flush
+    end
+
+    it "should split messages with multiple lines and send separately" do
+      Syslog.should_receive(:open).and_yield(syslog)
+      syslog.should_receive(:log).with(Syslog::LOG_WARNING, "Self destruction in 3...")
+      syslog.should_receive(:log).with(Syslog::LOG_WARNING, "2..")
+      syslog.should_receive(:log).with(Syslog::LOG_WARNING, "1.")
+      subject.error "Self destruction in 3...\n2..\n1."
+      subject.flush
+    end
+  end
+
+  # TODO: test logger level
+end
